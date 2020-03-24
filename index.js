a21c68e3dea09a50aed48305eb7bef187908167d
Reza <rzgholizadeh@gmail.com>
2020-03-24 16:24:45 -0600

    feat: A test mini canvas app with random shapse

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log("hi");
+const {
+	Engine,
+	Render,
+	Runner,
+	World,
+	Bodies,
+	MouseConstraint,
+	Mouse
+} = Matter;
+
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+	// where we want to show the canvas in our page
+	element: document.body,
+	engine: engine,
+	options: {
+		// outline or solid fill for shapes
+		wireframes: false,
+		// size of the canvas element
+		width,
+		height
+	}
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+World.add(
+	world,
+	MouseConstraint.create(engine, {
+		mouse: Mouse.create(render.canvas)
+	})
+);
+
+// Walls
+const walls = [
+	Bodies.rectangle(400, 0, 800, 40, {
+		isStatic: true
+	}),
+	Bodies.rectangle(400, 600, 800, 40, {
+		isStatic: true
+	}),
+	Bodies.rectangle(0, 300, 40, 600, {
+		isStatic: true
+	}),
+	Bodies.rectangle(800, 300, 40, 600, {
+		isStatic: true
+	})
+];
+World.add(world, walls);
+
+// random shapes
+for (let i = 0; i < 50; i++) {
+	if (Math.random() > 0.5) {
+		World.add(
+			world,
+			Bodies.rectangle(Math.random() * width, Math.random() * height, 40, 40)
+		);
+	} else {
+		World.add(
+			world,
+			Bodies.circle(Math.random() * width, Math.random() * height, 40, {
+				render: {
+					fillStyle: "teal"
+				}
+			})
+		);
+	}
+}
